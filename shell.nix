826ca0440b454210d0984229ce810b9942d4ea7e
cedric-star <cdrc.wnsch@gmail.com>
2025-11-17 14:37:06 +0100

    aufgabe 9

--- shell.nix
+++ shell.nix
@@ -24,6 +24,11 @@ pkgs.mkShell {
   shellHook = ''
     export JAVA_HOME=${pkgs.jdk21}
     export PATH=$JAVA_HOME/bin:$PATH
-    echo "✅ Java dev shell ready — using $JAVA_HOME"
+    echo "Java dev shell ready — using $JAVA_HOME"
+    echo "starting idea-community..."
+    idea-community .
   '';
 }
+### Ausführen: 
+# nix-shell
+# idea-community .
